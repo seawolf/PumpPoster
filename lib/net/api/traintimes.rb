fc4feed83f77b51b9991ac7fc5f9bcdd7ce0e030
Ben Arnold <ben@seawolfsanctuary.com>
2014-08-02 18:39:50 +0100

    Add: an TrainTimes/RTT JSON fetching class

---
+++ lib/net/api/traintimes.rb
@@ -0,0 +1,42 @@
+require 'date'
+
+require './lib/net/communicator.rb'
+
+module Api
+  module TrainTimes
+    HOST = "http://api.traintimes.im"
+
+    class Schedule
+      attr_reader :results
+
+      def initialize(uid, date)
+        @uid = uid
+
+        date = Date.parse(date) unless date.is_a?(Date)
+        @date = date
+
+        response = ::Communicator.get("#{HOST}/schedule_full.json?uid=#{@uid}&date=#{@date.strftime("%Y-%m-%d")}", 80)
+        @results = JSON.parse(response.body)
+
+        cleanup!
+      end
+
+      private
+
+      def cleanup!
+        # departure times: 1234 to 12:34
+        clean_time(@results["origin"]["departure_time"])
+        clean_time(@results["destination"]["arrival_time"])
+
+        @results["locations"].each do |c|
+          clean_time(c["departure_time"])
+          clean_time(c["arrival_time"])
+        end
+      end
+
+      def clean_time(str)
+        str.insert(2, ":") if str.is_a?(String)
+      end
+    end
+  end
+end
