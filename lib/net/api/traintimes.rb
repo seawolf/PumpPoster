8392fbc84f4dcccffb481850aea504900e4cdefd
Ben Arnold <ben@seawolfsanctuary.com>
2015-01-21 14:22:28 +0000

    Add: padding utility for RTT times

--- lib/net/api/traintimes.rb
+++ lib/net/api/traintimes.rb
@@ -16,8 +16,7 @@ module Api
 
         date = Date.parse(date) unless date.is_a?(Date)
         @date = date
-
-        response = ::Communicator.get("#{HOST}/schedule_full.json?uid=#{@uid}&date=#{@date.strftime("%Y-%m-%d")}", 80)
+        response = ::Communicator.get("#{HOST}/#{TYPE}/service/#{@uid}/#{pad(@date.year, 4)}/#{pad(@date.month)}/#{pad(@date.day)}", 443, nil, nil, fetch_creds)
         @results = response.code.to_i == 200 ? JSON.parse(response.body) : []
 
         cleanup! unless @results.empty?
@@ -27,17 +26,21 @@ module Api
 
       def cleanup!
         # departure times: 1234 to 12:34
-        clean_time(@results["origin"]["departure_time"])
-        clean_time(@results["destination"]["arrival_time"])
+        clean_time(@results['origin'][0]['publicTime'])
+        clean_time(@results['destination'][0]['publicTime'])
 
-        @results["locations"].each do |c|
-          clean_time(c["departure_time"])
-          clean_time(c["arrival_time"])
+        @results['locations'].each do |c|
+          clean_time(c['gbttBookedArrival'])
+          clean_time(c['gbttBookedDeparture'])
         end
       end
 
       def clean_time(str)
         str.insert(2, ":") if str.is_a?(String) && str.length >= 4
+      end
+
+      def pad(obj, length=2)
+        return obj.to_s.rjust(length, "0")
       end
 
       def fetch_creds
